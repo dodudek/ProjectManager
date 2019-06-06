a7342a4ce44b72f77ee5f01c8303cb3013a2f460
Dominika <dominikadudek30@gmail.com>
2019-06-06 15:40:23 +0200

    Projekt angularowy - część 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import {FormsModule} from "@angular/forms";
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ProjectDetailComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/mock-projects.ts
@@ -0,0 +1,7 @@
+import {Project} from "./project";
+
+export const PROJECTS: Project[] =[
+  {id: 3, name: 'Social media asset'},
+  {id: 4, name: 'Website redesign'},
+  {id: 5, name: 'Brand ad campaign'}
+];
